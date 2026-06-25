9984e93be720f7307e2bfeff7b03c7fd12dd171a
H.Nishiyama <55919851+aujinen@users.noreply.github.com>
2026-06-25 10:50:06 +0900

    Add files via upload

---
+++ workspace/test2D.py
@@ -0,0 +1,19 @@
+import os
+
+import SimpleITK as sitk
+import six
+
+from radiomics import featureextractor, getTestCase
+
+dataDir = './'
+
+imageName = os.path.join(dataDir, 'forRadiomicsTest.nrrd')
+maskName = os.path.join(dataDir, 'Segmentation.seg.nrrd')
+
+params = os.path.join(dataDir, '2D_sample_bin4.yaml')
+
+extractor = featureextractor.RadiomicsFeatureExtractor(params)
+
+results = extractor.execute(imageName, maskName)
+for key, val in six.iteritems(results):
+    print("\t%s: %s" % (key, val))
